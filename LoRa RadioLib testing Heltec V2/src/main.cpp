68bd1f37d8ea56a2860a615d405abb88968a1b79
lukass16 <lukasskellijs@gmail.com>
2023-06-03 11:04:06 +0300

    Develop LoRa 127x wrapper (Heltec)

--- LoRa RadioLib testing Heltec V2/src/main.cpp
+++ LoRa RadioLib testing Heltec V2/src/main.cpp
@@ -1,136 +1,17 @@
-// // include the library
 #include <Arduino.h>
-#include <RadioLib.h>
-
-#define SS 18
-#define RST 14
-#define DIO0 26
-#define DIO1 35
-#define SCK 5
-#define MISO 19
-#define MOSI 27
-
-SX1276 radio = new Module(SS, DIO0, RST, DIO1);
+#include "lora_127x_wrapper.h"
 
 void setup()
 {
-  Serial.begin(115200);
+	Serial.begin(115200);
 
-  // SPI LoRa pins
-  SPI.begin(SCK, MISO, MOSI, SS);
-
-  // initialize SX1278 with default settings
-  Serial.print(F("[SX1276] Initializing ... "));
-  int state = radio.begin(434, 125, 9, 7, 0x34, 10);
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    Serial.println(F("success!"));
-  }
-  else
-  {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true)
-      ;
-  }
+	lora::setup();
 }
 
 void loop()
 {
-  /*
-
-  String str;
-  int state = radio.receive(str);
-
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    // packet was successfully received
-    Serial.println(F("success!"));
-
-    // print the data of the packet
-    Serial.print(F("[SX1276] Data:\t\t\t"));
-    Serial.println(str);
-
-    // print the RSSI (Received Signal Strength Indicator)
-    // of the last received packet
-    Serial.print(F("[SX1276] RSSI:\t\t\t"));
-    Serial.print(radio.getRSSI());
-    Serial.println(F(" dBm"));
-
-    // print the SNR (Signal-to-Noise Ratio)
-    // of the last received packet
-    Serial.print(F("[SX1276] SNR:\t\t\t"));
-    Serial.print(radio.getSNR());
-    Serial.println(F(" dB"));
-
-    // print frequency error
-    // of the last received packet
-    Serial.print(F("[SX1276] Frequency error:\t"));
-    Serial.print(radio.getFrequencyError());
-    Serial.println(F(" Hz"));
-  }
-  else if (state == RADIOLIB_ERR_RX_TIMEOUT)
-  {
-    // timeout occurred while waiting for a packet
-    Serial.println(F("timeout!"));
-  }
-  else if (state == RADIOLIB_ERR_CRC_MISMATCH)
-  {
-    // packet was received, but is malformed
-    Serial.println(F("CRC error!"));
-  }
-  else
-  {
-    // some other error occurred
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-  }
-  */
-
-
-  Serial.print(F("[SX1278] Transmitting packet ... "));
-
-  // you can transmit C-string or Arduino string up to
-  // 256 characters long
-  // NOTE: transmit() is a blocking method!
-  //       See example SX127x_Transmit_Interrupt for details
-  //       on non-blocking transmission method.
-  int state = radio.transmit("Hello World!");
-
-  // you can also transmit byte array up to 256 bytes long
-  /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-    int state = radio.transmit(byteArr, 8);
-  */
-
-  if (state == RADIOLIB_ERR_NONE) {
-    // the packet was successfully transmitted
-    Serial.println(F(" success!"));
-
-    // print measured data rate
-    Serial.print(F("[SX1278] Datarate:\t"));
-    Serial.print(radio.getDataRate());
-    Serial.println(F(" bps"));
-
-  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
-    // the supplied packet was longer than 256 bytes
-    Serial.println(F("too long!"));
-
-  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-    // timeout occurred while transmitting packet
-    Serial.println(F("timeout!"));
-
-  } else {
-    // some other error occurred
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-
-  }
-
-  // wait for a second before transmitting again
-  delay(1000);
+	lora::send("Gerard", 1);
 }
-
 
 // //* Receiving messages
 // #include <Arduino.h>
@@ -144,7 +25,6 @@ void loop()
 // void loop() {
 //   lora::readMessage();
 // }
-
 
 //* Channel activity
 /*
