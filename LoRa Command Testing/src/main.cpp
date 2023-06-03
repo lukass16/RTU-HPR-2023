1a118443085d68f6a77f570b14596bd420313b96
lukass16 <lukasskellijs@gmail.com>
2023-06-03 10:38:20 +0300

    Develop LoRa 126x wrapper
    When receiving encounter status code -705 bug

--- LoRa Command Testing/src/main.cpp
+++ LoRa Command Testing/src/main.cpp
@@ -1,59 +1,17 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <RadioLib.h>
-
-// https://github.com/JJJS777/Hello_World_RadioLib_Heltec-V3_SX1262/blob/main/examples/LoRa_Transmitter/radioLib_SX1262_tx_example.cpp
-
-#define MOSI 10
-#define MISO 11
-#define SCK 9
-
-#define SS 8
-#define DIO1 14
-#define RST 12
-#define BUSY 13
-
-/*
-   RadioLib SX126x Transmit Example
-   This example transmits packets using SX1262 LoRa radio module.
-   Each packet contains up to 256 bytes of data, in the form of:
-    - Arduino String
-    - null-terminated char array (C-string)
-    - arbitrary binary data (byte array)
-   Other modules from SX126x family can also be used.
-   For default module settings, see the wiki page
-   https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx126x---lora-modem
-   For full API reference, see the GitHub Pages
-   https://jgromes.github.io/RadioLib/
-*/
-
-// SX1262 has the following connections:
-// NSS pin:   10
-// DIO1 pin:  2
-// NRST pin:  3
-// BUSY pin:  9
-
-
-// or using RadioShield
-// https://github.com/jgromes/RadioShield
-// SX1262 radio = RadioShield.ModuleA;
-
-// or using CubeCell
-// SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
+#include "lora_126x_wrapper.h"
 
 void setup()
 {
-  Serial.begin(115200);
-
-
+	Serial.begin(115200);
+	lora::setup();
 }
 
 void loop()
 {
-  
-  int state = radio.transmit("Lullaby");  // can transmit C-string or Arduino string up to 256 characters long
+	//lora::send("Gerard", 1);
+	lora::receive();
 
-  // wait for a second before transmitting again
-  delay(1000);
+	// wait for a second before transmitting again
+	delay(1000);
 }
