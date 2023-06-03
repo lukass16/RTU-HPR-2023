32ee49d4f454157a000786c1992092f369a477a5
lukass16 <lukasskellijs@gmail.com>
2023-06-03 10:39:07 +0300

    Update Heltec V2 testing

--- LoRa RadioLib testing Heltec V2/src/main.cpp
+++ LoRa RadioLib testing Heltec V2/src/main.cpp
@@ -37,6 +37,7 @@ void setup()
 
 void loop()
 {
+  /*
 
   String str;
   int state = radio.receive(str);
@@ -84,6 +85,50 @@ void loop()
     Serial.print(F("failed, code "));
     Serial.println(state);
   }
+  */
+
+
+  Serial.print(F("[SX1278] Transmitting packet ... "));
+
+  // you can transmit C-string or Arduino string up to
+  // 256 characters long
+  // NOTE: transmit() is a blocking method!
+  //       See example SX127x_Transmit_Interrupt for details
+  //       on non-blocking transmission method.
+  int state = radio.transmit("Hello World!");
+
+  // you can also transmit byte array up to 256 bytes long
+  /*
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+    int state = radio.transmit(byteArr, 8);
+  */
+
+  if (state == RADIOLIB_ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F(" success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1278] Datarate:\t"));
+    Serial.print(radio.getDataRate());
+    Serial.println(F(" bps"));
+
+  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+
+  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+    // timeout occurred while transmitting packet
+    Serial.println(F("timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+
+  // wait for a second before transmitting again
+  delay(1000);
 }
 
 
