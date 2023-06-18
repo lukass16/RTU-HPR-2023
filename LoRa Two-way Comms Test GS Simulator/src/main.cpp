68d9c7f743f02a9b38d3bdc561422d794fd3b513
lukass16 <lukasskellijs@gmail.com>
2023-06-18 16:04:08 +0300

    Implement Sync Time in GS Two-way Comms Test

--- LoRa Two-way Comms Test GS Simulator/src/main.cpp
+++ LoRa Two-way Comms Test GS Simulator/src/main.cpp
@@ -2,6 +2,10 @@
 #include "lora_127x_wrapper.h"
 #include "gps_wrapper.h"
 
+#define GPS_TIME_TRIGGER 2
+
+int syncTime = 0;
+
 void setup()
 {
 	Serial.begin(115200);
@@ -14,5 +18,13 @@ void loop()
 {
 	//lora::pingPong();
 	gps::readGps();
-	gps::printTime();
+	if(gps::getSyncTime() != syncTime)
+	{
+		syncTime = gps::getSyncTime();
+		Serial.println("Sync time: " + String(syncTime));
+		if(syncTime == GPS_TIME_TRIGGER)
+		{
+			Serial.println("SEND triggered.");
+		}
+	}
 }
